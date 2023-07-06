5eabcbad4a2d363e0ef23f6488d1cf8fbd4c2d73
hide001 <shubhamswaraj1@gmail.com>
2023-07-06 17:18:52 +0530

    update

--- format-package.sh
+++ format-package.sh
@@ -32,7 +32,7 @@ task1(){
   echo -e "Are you sure want to continue and completly format & delte all the existing node installations? (Y/N)"
   read input
   
-  if [ [$input == "y"] ]; then
+  if [ [$input == 'y'] ]; then
         echo "${RED} Format in progress..."
         cd ./chaindata
         rm -rf ./*
