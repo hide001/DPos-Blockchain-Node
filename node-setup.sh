5d8c8e0a441c09fdceebf03a0e5b3d8f5566a064
hide001 <shubhamswaraj1@gmail.com>
2023-07-06 16:23:46 +0530

    update node-setup.sh

--- node-setup.sh
+++ node-setup.sh
@@ -14,6 +14,126 @@ totalNodes=$(($totalRpc + $totalValidator))
 #########################################################################
 
 #+-----------------------------------------------------------------------------------------------+
+#|                                                                                                                              |
+#|                                                      FUNCTIONS                                                |
+#|                                                                                                                              |     
+#+------------------------------------------------------------------------------------------------+
+
+task1(){
+  # update and upgrade the server TASK 1
+  echo -e "\n\n${ORANGE}TASK: ${GREEN}[running upgrade]${NC}\n"
+  apt update && apt upgrade -y
+  echo -e "\n${GREEN}[TASK 1 PASSED]${NC}\n"
+}
+
+task2(){
+  # installing build-essential TASK 2
+  echo -e "\n${ORANGE}TASK: ${GREEN}[installing build-essential]${NC}\n"
+  apt -y install build-essential tree
+  echo -e "\n${GREEN}[TASK 2 PASSED]${NC}\n"
+}
+
+task3(){
+  # getting golang TASK 3
+  echo -e "\n${ORANGE}TASK: ${GREEN}[getting golang v 1.17.3]${NC}\n"
+  cd ./tmp && wget "https://go.dev/dl/go1.17.3.linux-amd64.tar.gz"
+  echo -e "\n${GREEN}[TASK 3 PASSED]${NC}\n"
+}
+
+task4(){
+  # setting up golang TASK 4
+  echo -e "\n${ORANGE}TASK: ${GREEN}[installing golang]${NC}\n"
+  rm -rf /usr/local/go && tar -C /usr/local -xzf go1.17.3.linux-amd64.tar.gz
+  echo "PATH=$PATH:/usr/local/go/bin" >>/etc/profile
+  export PATH=$PATH:/usr/local/go/bin
+  go env -w GO111MODULE=off
+  echo -e "\n${GREEN}[TASK 4 PASSED]${NC}\n"
+}
+
+task5(){
+  # set proper group and permissions TASK 5
+  echo -e "\n${ORANGE}TASK: ${GREEN}[setting up group and permissions]${NC}\n"
+  ls -all
+  cd ../
+  ls -all
+  chown -R root:root ./
+  chmod a+x ./node-start.sh
+  echo -e "\n${GREEN}[TASK 5 PASSED]${NC}\n"
+}
+
+task6(){
+  # do make all TASK 6
+  echo -e "\n${ORANGE}TASK: ${GREEN}[making all]${NC}\n"
+  cd node_src
+  make all
+  echo -e "\n${GREEN}[TASK 6 PASSED]${NC}\n"
+}
+
+task7(){
+  # setting up directories and structure for node/s TASK 7
+  echo -e "\n${ORANGE}TASK: ${GREEN}[setting up directories]${NC}\n"
+
+  cd ../
+
+  i=1
+  while [[ $i -le $totalNodes ]]; do
+    mkdir ./chaindata/node$i
+    ((i += 1))
+  done
+
+  tree ./chaindata
+  echo -e "\n${GREEN}[TASK 7 PASSED]${NC}\n"
+}
+
+task8(){
+  #TASK 8
+  echo -e "\n${ORANGE}TASK: ${GREEN}[getting password for account]${NC}\n"
+  echo -e "\n${ORANGE}This step is very important. Input a password that will be used for a newly created validator account. In next step you will recieve a public key for your validator account"
+  echo -e "${ORANGE}Once a validator account is created using your given password, I'll give you where the keystore file is located so you can import it in metamask\n\n${NC}"
+
+  i=1
+  while [[ $i -le $totalValidator ]]; do
+
+    echo -e "Enter password for validator $i: "
+    read password
+    echo $password >./chaindata/node$i/pass.txt
+    ./node_src/build/bin/geth --datadir ./chaindata/node$i account new --password ./chaindata/node$i/pass.txt
+    ./node_src/build/bin/geth --datadir ./chaindata/node$i init ./genesis.json
+    ((i += 1))
+  done
+
+  echo -e "\n${GREEN}[TASK 8 PASSED]${NC}\n"
+}
+
+displayStatus(){
+  # start the node
+  echo -e "\n Your newly created account and password is located at ./chaindata/node1"
+  echo -e "\n${ORANGE}STATUS: ${GREEN}ALL TASK PASSED!\n This program will now exit\n Now run ./node-start.sh${NC}\n"
+}
+
+displayWelcome(){
+  # display welcome message
+  echo -e "\t${ORANGE}Total RPC to be created: $totalRpc"
+  echo -e "\t${ORANGE}Total Validators to be created: $totalValidator"
+  echo -e "\t${ORANGE}Total nodes to be created: $totalNodes"
+  echo -e "${GREEN}
+  \t+------------------------------------------------+
+  \t|   DPos node installation Wizard
+  \t|   Target OS: Ubuntu 20.04 LTS (Focal Fossa)
+  \t|   Your OS: $(. /etc/os-release && printf '%s\n' "${PRETTY_NAME}") 
+  \t+------------------------------------------------+
+  ${NC}\n"
+
+  echo -e "${ORANGE}
+  \t+------------------------------------------------+
+  \t+------------------------------------------------+
+  ${NC}"
+}
+
+
+#########################################################################
+
+#+-----------------------------------------------------------------------------------------------+
 #|                                                                                                                             |
 #|                                                                                                                             |
 #|                                                      UTILITY                                                        |
@@ -149,122 +269,3 @@ fi
 
 
 
-#+-----------------------------------------------------------------------------------------------+
-#|                                                                                                                              |
-#|                                                      FUNCTIONS                                                |
-#|                                                                                                                              |     
-#+------------------------------------------------------------------------------------------------+
-
-task1(){
-  # update and upgrade the server TASK 1
-  echo -e "\n\n${ORANGE}TASK: ${GREEN}[running upgrade]${NC}\n"
-  apt update && apt upgrade -y
-  echo -e "\n${GREEN}[TASK 1 PASSED]${NC}\n"
-}
-
-task2(){
-  # installing build-essential TASK 2
-  echo -e "\n${ORANGE}TASK: ${GREEN}[installing build-essential]${NC}\n"
-  apt -y install build-essential tree
-  echo -e "\n${GREEN}[TASK 2 PASSED]${NC}\n"
-}
-
-task3(){
-  # getting golang TASK 3
-  echo -e "\n${ORANGE}TASK: ${GREEN}[getting golang v 1.17.3]${NC}\n"
-  cd ./tmp && wget "https://go.dev/dl/go1.17.3.linux-amd64.tar.gz"
-  echo -e "\n${GREEN}[TASK 3 PASSED]${NC}\n"
-}
-
-task4(){
-  # setting up golang TASK 4
-  echo -e "\n${ORANGE}TASK: ${GREEN}[installing golang]${NC}\n"
-  rm -rf /usr/local/go && tar -C /usr/local -xzf go1.17.3.linux-amd64.tar.gz
-  echo "PATH=$PATH:/usr/local/go/bin" >>/etc/profile
-  export PATH=$PATH:/usr/local/go/bin
-  go env -w GO111MODULE=off
-  echo -e "\n${GREEN}[TASK 4 PASSED]${NC}\n"
-}
-
-task5(){
-  # set proper group and permissions TASK 5
-  echo -e "\n${ORANGE}TASK: ${GREEN}[setting up group and permissions]${NC}\n"
-  ls -all
-  cd ../
-  ls -all
-  chown -R root:root ./
-  chmod a+x ./node-start.sh
-  echo -e "\n${GREEN}[TASK 5 PASSED]${NC}\n"
-}
-
-task6(){
-  # do make all TASK 6
-  echo -e "\n${ORANGE}TASK: ${GREEN}[making all]${NC}\n"
-  cd node_src
-  make all
-  echo -e "\n${GREEN}[TASK 6 PASSED]${NC}\n"
-}
-
-task7(){
-  # setting up directories and structure for node/s TASK 7
-  echo -e "\n${ORANGE}TASK: ${GREEN}[setting up directories]${NC}\n"
-
-  cd ../
-
-  i=1
-  while [[ $i -le $totalNodes ]]; do
-    mkdir ./chaindata/node$i
-    ((i += 1))
-  done
-
-  tree ./chaindata
-  echo -e "\n${GREEN}[TASK 7 PASSED]${NC}\n"
-}
-
-task8(){
-  #TASK 8
-  echo -e "\n${ORANGE}TASK: ${GREEN}[getting password for account]${NC}\n"
-  echo -e "\n${ORANGE}This step is very important. Input a password that will be used for a newly created validator account. In next step you will recieve a public key for your validator account"
-  echo -e "${ORANGE}Once a validator account is created using your given password, I'll give you where the keystore file is located so you can import it in metamask\n\n${NC}"
-
-  i=1
-  while [[ $i -le $totalValidator ]]; do
-
-    echo -e "Enter password for validator $i: "
-    read password
-    echo $password >./chaindata/node$i/pass.txt
-    ./node_src/build/bin/geth --datadir ./chaindata/node$i account new --password ./chaindata/node$i/pass.txt
-    ./node_src/build/bin/geth --datadir ./chaindata/node$i init ./genesis.json
-    ((i += 1))
-  done
-
-  echo -e "\n${GREEN}[TASK 8 PASSED]${NC}\n"
-}
-
-displayStatus(){
-  # start the node
-  echo -e "\n Your newly created account and password is located at ./chaindata/node1"
-  echo -e "\n${ORANGE}STATUS: ${GREEN}ALL TASK PASSED!\n This program will now exit\n Now run ./node-start.sh${NC}\n"
-}
-
-displayWelcome(){
-  # display welcome message
-  echo -e "\t${ORANGE}Total RPC to be created: $totalRpc"
-  echo -e "\t${ORANGE}Total Validators to be created: $totalValidator"
-  echo -e "\t${ORANGE}Total nodes to be created: $totalNodes"
-  echo -e "${GREEN}
-  \t+------------------------------------------------+
-  \t|   DPos node installation Wizard
-  \t|   Target OS: Ubuntu 20.04 LTS (Focal Fossa)
-  \t|   Your OS: $(. /etc/os-release && printf '%s\n' "${PRETTY_NAME}") 
-  \t+------------------------------------------------+
-  ${NC}\n"
-
-  echo -e "${ORANGE}
-  \t+------------------------------------------------+
-  \t+------------------------------------------------+
-  ${NC}"
-}
-
-
-#########################################################################
